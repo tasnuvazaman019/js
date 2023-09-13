d8b0161b6a1504662b7cea5965c9ecffd8508cbb
Tasnuva Zaman <68296247+tasnuvazaman019@users.noreply.github.com>
2023-09-13 12:21:49 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function textChange(){
+	document.getElementById('h2').innerHTML="Assalamualaikum World";
+}
+
+function showDate(){
+	document.getElementById('date_h2').innerHTML = Date();
+}
+
+// bulb onn off
+function bulbOn(){
+	document.getElementById('img').src = "images/on.gif";
+}
+
+function bulbOff(){
+	document.getElementById('img').src = "images/off.gif";
+}
